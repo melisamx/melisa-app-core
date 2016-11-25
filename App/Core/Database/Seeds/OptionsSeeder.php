25fc7e4166d48329319a7f16ae7b1aa2d7ab90e1
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-24 23:27:02 -0600

    add option tbfill

---
+++ App/Core/Database/Seeds/OptionsSeeder.php
@@ -0,0 +1,23 @@
+<?php namespace App\Core\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use App\Core\Models\Options;
+
+class OptionsSeeder extends Seeder
+{
+    
+    public function run()
+    {
+                
+        Options::updateOrCreate([
+            'key'=>'tbfill',
+        ], [
+            'name'=>'Sencha Component tbfill',
+            'iconClassSmall'=>null,
+            'iconClassMedium'=>null,
+            'iconClassLarge'=>null,
+        ]);
+        
+    }
+    
+}
