8d46b548d987d85b0fa2a025ab4e6364be310fa0
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-26 13:47:38 -0600

    add outbuildings menu

---
+++ App/Core/Logics/Modules/Outbuildings/Menu.php
@@ -0,0 +1,89 @@
+<?php namespace App\Core\Logics\Modules\Outbuildings;
+
+use Melisa\core\LogicBusiness;
+use App\Core\Logics\Menus\Hierarchical;
+
+/**
+ * 
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+class Menu
+{
+    use LogicBusiness;
+    
+    protected $menus;
+
+    public function __construct(Hierarchical $menus) {
+        
+        $this->menus = $menus;
+        
+    }
+    
+    public function get($keys = []) {
+        
+        if( is_string($keys)) {
+            
+            $keys = [ $keys ];
+            
+        }
+        
+        $this->debug('loading {c} menus: {i}', [
+            'c'=>count($keys),
+            'i'=>implode(',', $keys)
+        ]);
+        
+        $menus = [];
+        $flag = true;
+        
+        foreach($keys as $key) {
+            
+            $menu = $this->load($key);
+            
+            if( $menu) {
+                
+                $menus [$key]= $menu;
+                continue;
+                
+            }
+            
+            $flag = false;
+            break;
+            
+        }
+        
+        if( !$flag) {
+            
+            return null;
+            
+        }
+        
+        return $menus[$key];
+        
+    }
+    
+    public function load($key) {
+        
+        static $loades = [];
+        
+        if( isset($loades[$key])) {
+            
+            return $loades[$key];
+            
+        }
+        
+        $menu = $this->menus->generate($key);
+        
+        if( $menu === false) {
+
+            return false;
+
+        }
+        
+        $loades [$key]= $menu;
+
+        return $menu;
+        
+    }
+    
+}
