953797fa7a11e85c10d15a9b94be3f677bd0eccf
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-12-08 14:55:08 -0600

    implement use install asset

--- App/Core/Database/Seeds/AssetsCssSeeder.php
+++ App/Core/Database/Seeds/AssetsCssSeeder.php
@@ -1,66 +1,57 @@
 <?php namespace App\Core\Database\Seeds;
 
 use Illuminate\Database\Seeder;
-use Melisa\Laravel\Database\FirstOrCreate;
+use Melisa\Laravel\Database\InstallAsset;
+use Melisa\Laravel\Database\UpdateOrCreate;
 
 class AssetsCssSeeder extends Seeder
 {
     
-    use FirstOrCreate;
+    use InstallAsset, UpdateOrCreate;
     
     public function run()
     {
         
-        $this->firstOrCreate('App\Core\Models\Assets', [
-            [
-                'id'=>'fontawesome',
-                'idAssetType'=>2,
-                'name'=>'Fontawesome',
-                'path'=>'/vendor/fontawesome/release/css/font-awesome.min.css',
-                'cdn'=>'//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css',
-                'extraParams'=>'androidAsset=inject',
-            ],
-            [
-                'id'=>'animatecss',
-                'idAssetType'=>2,
-                'name'=>'Animate.css',
-                'path'=>'/vendor/animatecss/release/animate.min.css',
-                'extraParams'=>'androidAsset=inject',
-            ],
-            [
-                'id'=>'waves.css',
-                'idAssetType'=>2,
-                'name'=>'Waves Css',
-                'path'=>'/vendor/waves/release/waves.css',
-                'extraParams'=>'androidAsset=inject',
-            ],
-            [
-                'id'=>'waves.sencha',
-                'idAssetType'=>2,
-                'name'=>'Waves to Sencha',
-                'path'=>'/vendor/waves/release/sencha.css',
-                'extraParams'=>'androidAsset=inject',
-            ],
-            [
-                'id'=>'roboto',
-                'idAssetType'=>2,
-                'name'=>'Font Roboto',
-                'path'=>'/vendor/roboto/style.css',
-                'extraParams'=>'androidAsset=inject',
-            ],
-            [
-                'id'=>'bootstrap.reports',
-                'idAssetType'=>2,
-                'name'=>'Bootstrap for reports',
-                'path'=>'/vendor/bootstrap/release/reports/css/bootstrap.min.css',
-            ],
-            [
-                'id'=>'bootstrap.reports.print',
-                'idAssetType'=>2,
-                'name'=>'Bootstrap for reports and printers',
-                'path'=>'/vendor/bootstrap/release/reports/css/print.css',
-            ],
-            
+        $this->installAssetCss('fontawesome', [
+            'name'=>'Fontawesome',
+            'path'=>'/vendor/fontawesome/release/css/font-awesome.min.css',
+            'cdn'=>'//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css',
+            'extraParams'=>'androidAsset=inject',
+            'version'=>'4.7.0',
+        ]);
+        
+        $this->installAssetCss('animatecss', [
+            'name'=>'Animate.css',
+            'path'=>'/vendor/animatecss/release/animate.min.css',
+            'extraParams'=>'androidAsset=inject',
+        ]);
+        
+        $this->installAssetCss('waves.css', [
+            'name'=>'Waves Css',
+            'path'=>'/vendor/waves/release/waves.css',
+            'extraParams'=>'androidAsset=inject',
+        ]);
+        
+        $this->installAssetCss('waves.sencha', [
+            'name'=>'Waves to Sencha',
+            'path'=>'/vendor/waves/release/sencha.css',
+            'extraParams'=>'androidAsset=inject',
+        ]);
+        
+        $this->installAssetCss('roboto', [
+            'name'=>'Font Roboto',
+            'path'=>'/vendor/roboto/style.css',
+            'extraParams'=>'androidAsset=inject',
+        ]);
+        
+        $this->installAssetCss('bootstrap.reports', [
+            'name'=>'Bootstrap for reports',
+            'path'=>'/vendor/bootstrap/release/reports/css/bootstrap.min.css',
+        ]);
+        
+        $this->installAssetCss('bootstrap.reports.print', [
+            'name'=>'Bootstrap for reports and printers',
+            'path'=>'/vendor/bootstrap/release/reports/css/print.css',
         ]);
         
     }
