fe7e00770fcf3dac47c2cc9aa29e25ad9e4f0340
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-12-13 18:59:50 -0600

    added listener register event on binnacle

---
+++ App/Core/Listeners/EventBinnacleListener.php
@@ -0,0 +1,35 @@
+<?php namespace App\Core\Listeners;
+
+use App\Core\Logics\Binnacle\RegisterEvent;
+use Melisa\Laravel\Contracts\EventBinnacle;
+
+/**
+ * 
+ *
+ * @author Luis Josafat Heredia Contreras
+ */
+class EventBinnacleListener
+{
+    
+    protected $register;
+
+    public function __construct(RegisterEvent $register)
+    {
+        
+        $this->register = $register;
+        
+    }
+    
+    /**
+     * Handle the event.
+     *
+     * @return void
+     */
+    public function handle(EventBinnacle $event)
+    {
+        
+        return $this->register->init($event);
+        
+    }
+    
+}
