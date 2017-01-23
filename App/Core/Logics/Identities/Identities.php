99c096752b097874571cfcca8ca18fca79842424
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-01-22 20:20:56 -0600

    First tests with grouped security systems

--- App/Core/Logics/Identities/Identities.php
+++ App/Core/Logics/Identities/Identities.php
@@ -1,6 +1,7 @@
 <?php namespace App\Core\Logics\Identities;
 
 use App\Core\Logics\Identities\IdentitySession;
+use Melisa\core\LogicBusiness;
 
 /**
  * 
@@ -9,25 +10,42 @@ use App\Core\Logics\Identities\IdentitySession;
  */
 class Identities
 {
+    use LogicBusiness;
 
     public function get($idUser = null)
     {
         static $class = null;
         
         if( !$class) {
-            
             $class = app()->make(IdentitySession::class);
-            
-            
         }
         
         if( is_null($idUser)) {
             
-            $idUser = request()->user()->id;
+            $user = $this->getUser();
+            
+            if( !$user) {
+                return false;
+            }
+            
+            $idUser = $user->id;
             
         }
         
         return $class->init($idUser);
+        
+    }
+    
+    public function getUser()
+    {
+        
+        $user = request()->user();
+        
+        if( is_null($user)) {
+            return $this->error('User Unauthenticated');
+        }
+        
+        return $user;
         
     }
     
