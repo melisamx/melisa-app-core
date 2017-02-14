f5b5c0a7e5b0f4e826a2a4df1001aeb80f399970
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-02-13 19:38:52 -0600

    Fields were repeated because they were not filtered by database

--- App/Core/Console/Commands/ModelsGenerate.php
+++ App/Core/Console/Commands/ModelsGenerate.php
@@ -174,10 +174,8 @@ class ModelsGenerate extends GeneratorCommand
             
             $fields = $this->getFieldMetadata($connection, $table);
             
-            if( empty($fields)) {
-                
-                continue;
-                
+            if( empty($fields)) {    
+                continue;                
             }
             
             $struct [$table]= $this->prepareConfig($fields);
@@ -394,6 +392,7 @@ class ModelsGenerate extends GeneratorCommand
     {
         
         $prefix = $connection->getConfig('prefix');
+        $database = $connection->getConfig('database');
         
         if( empty($prefix)) {
             
@@ -402,7 +401,9 @@ class ModelsGenerate extends GeneratorCommand
         }
         
         /* extract to http://lucidchart.com/ */
-        $columns = $connection->select('SELECT c.COLUMN_NAME, c.ORDINAL_POSITION, c.DATA_TYPE, c.CHARACTER_MAXIMUM_LENGTH, c.COLUMN_DEFAULT, c.IS_NULLABLE, c.NUMERIC_PRECISION, c.NUMERIC_SCALE, n.CONSTRAINT_TYPE, k.REFERENCED_TABLE_SCHEMA, k.REFERENCED_TABLE_NAME, k.REFERENCED_COLUMN_NAME, t.AUTO_INCREMENT         FROM INFORMATION_SCHEMA.TABLES t         LEFT JOIN INFORMATION_SCHEMA.COLUMNS c ON t.TABLE_SCHEMA=c.TABLE_SCHEMA AND t.TABLE_NAME=c.TABLE_NAME         LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON c.TABLE_SCHEMA=k.TABLE_SCHEMA AND c.TABLE_NAME=k.TABLE_NAME AND c.COLUMN_NAME=k.COLUMN_NAME         LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS n ON k.CONSTRAINT_SCHEMA=n.CONSTRAINT_SCHEMA AND k.CONSTRAINT_NAME=n.CONSTRAINT_NAME AND k.TABLE_SCHEMA=n.TABLE_SCHEMA AND k.TABLE_NAME=n.TABLE_NAME         WHERE t.TABLE_TYPE=\'BASE TABLE\' AND t.TABLE_NAME = "' . $table . '"');
+        $columns = $connection->select('SELECT c.TABLE_SCHEMA, c.COLUMN_NAME, c.ORDINAL_POSITION, c.DATA_TYPE, c.CHARACTER_MAXIMUM_LENGTH, c.COLUMN_DEFAULT, c.IS_NULLABLE, c.NUMERIC_PRECISION, c.NUMERIC_SCALE, n.CONSTRAINT_TYPE, k.REFERENCED_TABLE_SCHEMA, k.REFERENCED_TABLE_NAME, k.REFERENCED_COLUMN_NAME, t.AUTO_INCREMENT         FROM INFORMATION_SCHEMA.TABLES t         LEFT JOIN INFORMATION_SCHEMA.COLUMNS c ON t.TABLE_SCHEMA=c.TABLE_SCHEMA AND t.TABLE_NAME=c.TABLE_NAME         LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE k ON c.TABLE_SCHEMA=k.TABLE_SCHEMA AND c.TABLE_NAME=k.TABLE_NAME AND c.COLUMN_NAME=k.COLUMN_NAME         LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS n ON k.CONSTRAINT_SCHEMA=n.CONSTRAINT_SCHEMA AND k.CONSTRAINT_NAME=n.CONSTRAINT_NAME AND k.TABLE_SCHEMA=n.TABLE_SCHEMA AND k.TABLE_NAME=n.TABLE_NAME         WHERE t.TABLE_TYPE=\'BASE TABLE\' AND t.TABLE_NAME = "' . 
+                $table . '" AND c.TABLE_SCHEMA="' .
+                $database . '"');
         
         $fieldColumn = [];
         
