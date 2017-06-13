be9709f9d7cb21b1c7925941073e1429c0a2ffe1
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-06-13 12:33:32 -0500

    use kernel secure

--- bootstrap/core.php
+++ bootstrap/core.php
@@ -34,7 +34,7 @@ $app = new App\Core\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    Melisa\Laravel\Http\Kernel::class
+    Melisa\Laravel\Http\KernelSecure::class
 );
 
 $app->singleton(
